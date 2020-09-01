1a5d1021c9ea8eeec2bfb56949f16084dea3e04b
Cailie Skelton <cailie.skelton86@gmail.com>
2020-09-01 23:38:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
-  # write code here
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
+    else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
+    end
+    #if @side_1 == 0 && @side_2 == 0 && @side_3 == 0
+     # raise.new TriangleError
+    #end
+  end
+
+end
+
+ def validate_Triangle
+    real_triangle = [(side_1 + side_2 > side_3), (side_1 + side_3 > side_2), (side_2 + side_3 > side_1)]
+    [side_1, side_2, side_3].each {|s| real_triangle << false if s<= 0}
+    raise TriangleError if real_triangle.include?(false)
+   end
+
+class TriangleError < StandardError
+  def message
+    puts "not a triangle"
+  end
 end
